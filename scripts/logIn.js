56c14ffd4ddec1490890b0972af02dcd303b2a5e
shafikulnayem10 <inayem98@gmail.com>
2026-02-17 22:44:09 +0600

    first commit

---
+++ scripts/logIn.js
@@ -0,0 +1,23 @@
+let logInBtn = document.getElementById("login-btn");
+logInBtn.addEventListener("click", function() {
+ //1- get the mobile number input
+  const numberInput = document.getElementById("input-number");
+  const contactNumber = numberInput.value;
+  console.log(contactNumber);
+  //2- get the pin input
+  const inputPin = document.getElementById("input-pin");
+  const pin = inputPin.value;
+  console.log(pin);
+    //3- match pin & mobile number
+  if (contactNumber == "01234567890" && pin == "1234") {
+    
+    
+
+   
+    window.location.assign("/home.html");
+  } else {
+   
+    alert("login Failed");
+    return;
+  }
+})
